f438d7e5eb3ca1268ee1b37eadc17097fe55e8cc
ShivAgrahari <100069321+ShivAgrahari@users.noreply.github.com>
2024-07-24 22:14:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,16 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
 ps = PorterStemmer()
 
 
